2a7db54452342b12044e1a9b042f325410116ce5
wkimunya <136619749+wkimunya@users.noreply.github.com>
2023-10-14 14:25:23 +0300

    complete lab

--- lib/dog.py
+++ lib/dog.py
@@ -2,6 +2,14 @@
 
 class Dog:
     # Class body goes here
+ 
 
     #Instance method definition
-    pass
+ def bark(self):
+    print('Woof!')
+ 
+ def sit(self):
+   print('The dog is sitting.')
+
+
+
